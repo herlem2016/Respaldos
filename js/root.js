d1eb286fcee848e33ad9bafb143ed4ee8a8bd00c
herlem2016 <herlem.kx@gmail.com>
2021-11-12 20:37:52 -0600

    Add files via upload

--- js/root.js
+++ js/root.js
@@ -12,26 +12,28 @@ function CrearDom(tag,contenido){
 	return nuevo_dom;
 }
 
-var GetVal= function(nodo,tag_nombre){
-	return $($(nodo).find(tag_nombre)[0]).text();
+var GetValAttr= function(nodo,attr){
+	return $(nodo).attr(attr);
 }
 
 function decodeHTMLEntities(text) {
-    var entities = [
-        ['amp', '&'],
-        ['apos', '\''],
-        ['#x27', '\''],
-        ['#x2F', '/'],
-        ['#39', '\''],
-        ['#47', '/'],
-        ['lt', '<'],
-        ['gt', '>'],
-        ['nbsp', ' '],
-        ['quot', '"']
-    ];
+	if(text){
+		var entities = [
+			['amp', '&'],
+			['apos', '\''],
+			['#x27', '\''],
+			['#x2F', '/'],
+			['#39', '\''],
+			['#47', '/'],
+			['lt', '<'],
+			['gt', '>'],
+			['nbsp', ' '],
+			['quot', '"']
+		];
 
-    for (var i = 0, max = entities.length; i < max; ++i) 
-        text = text.replace(new RegExp('&'+entities[i][0]+';', 'g'), entities[i][1]);
+		for (var i = 0, max = entities.length; i < max; ++i) 
+			text = text.replace(new RegExp('&'+entities[i][0]+';', 'g'), entities[i][1]);
+	}
 
     return text;
 }
@@ -48,7 +50,7 @@ _ROOT_SYSTEM.ConstruirUI= function(url,proyecto,dom_contenedor){
 
 _ROOT_SYSTEM.ProcesarArbolUnidadesUI= function(xmlinfo){
 	var nuevo_dom;
-	var unidadesUI=$(xmlinfo).find("> NewDataSet > Table");
+	var unidadesUI=$(xmlinfo).find("ui > unidadUI");
 	if(unidadesUI.length==0){
 		nuevo_dom=_ROOT_SYSTEM.ResolverUnaUnidadUI(xmlinfo);
 	}else {
@@ -61,19 +63,27 @@ _ROOT_SYSTEM.ProcesarArbolUnidadesUI= function(xmlinfo){
 }
 
 _ROOT_SYSTEM.ResolverUnaUnidadUI= function(xmlUnidadUI){
-	var style=GetVal(xmlUnidadUI,"css");if(style.length>0) document.head.appendChild(CrearDom("style",style));
-	var script=GetVal(xmlUnidadUI,"javascript");if(script.length>0) document.head.appendChild(CrearDom("script",script.length>0?script:""));
+	var unTipoUI=$(xmlUnidadUI).find("tipoUnidadUI")[0];
+	var style=GetValAttr(unTipoUI,"css");if(style) document.head.appendChild(CrearDom("style",style));
+	var script=GetValAttr(unTipoUI,"javascript");if(script) document.head.appendChild(CrearDom("script",script.length>0?script:""));
 	var nuevo_dom= document.createElement("div");	
-	nuevo_dom.className= GetVal(xmlUnidadUI,"class");
-	nuevo_dom.innerHTML=decodeHTMLEntities(GetVal(xmlUnidadUI,"innerHTML"));
-	/*nuevo_dom.style= GetVal(xmlinfo,"evento");
+	nuevo_dom.className= GetValAttr(unTipoUI,"class");
+	nuevo_dom.innerHTML=decodeHTMLEntities(GetValAttr(unTipoUI,"innerHTML"));
+	//Obtenemos las propiedades del TipoUI
+	var propiedades=$(unTipoUI).find("propiedad");
+	for(var i=0;i<propiedades.length;i++){
+		$.each($(nuevo_dom).find("[em-id='" + propiedades[i].getAttribute("descripcion") + "']"), function(index,obj){
+			obj.innerHTML=propiedades[i].children[0].getAttribute("valor");
+		});
+	}
+	/*nuevo_dom.style= GetValAttr(xmlinfo,"evento");
 	var atributos=$(xmlinfo).find(" > atributo");
 	for(var i=0;i<atributos.length;i++){
-		nuevo_dom.setAttribute(GetVal(atributos[i],"nombre"),GetVal(atributos[i],"valor"));
+		nuevo_dom.setAttribute(GetValAttr(atributos[i],"nombre"),GetValAttr(atributos[i],"valor"));
 	}
 	var eventos=$(xmlinfo).find(" > eventos");
 	for(var i=0;i<eventos.length;i++){
-		nuevo_dom.addEventListener(GetVal(eventos[i],"evento"),GetVal(eventos[i],"metodo"));
+		nuevo_dom.addEventListener(GetValAttr(eventos[i],"evento"),GetValAttr(eventos[i],"metodo"));
 	}*/
 	return nuevo_dom;
 }
