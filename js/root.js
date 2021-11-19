bd8e3c6eaf6e2a711a331cfb680a3e40894004ba
herlem2016 <herlem.kx@gmail.com>
2021-11-18 21:01:09 -0600

    Add files via upload

--- js/root.js
+++ js/root.js
@@ -62,6 +62,10 @@ _ROOT_SYSTEM.ProcesarArbolUnidadesUI= function(xmlinfo){
 	return nuevo_dom;
 }
 
+_ROOT_SYSTEM.CargarInfoUnidadUI= function(sdiv_repeat, opx){
+	$.post({url:"controlador.aspx?op=ObtenerOPX&seccion=generic&opx=" + opx, succes:function(data,xqr){ eval("Fn_Repeat_" + sdiv_repeat + "_" + opx + "(data,xqr,$('#" + sdiv_repeat + "'));"); }});
+}
+
 _ROOT_SYSTEM.ResolverUnaUnidadUI= function(xmlUnidadUI){
 	var unTipoUI=$(xmlUnidadUI).find("tipoUnidadUI")[0];
 	var style=GetValAttr(unTipoUI,"css");if(style) document.head.appendChild(CrearDom("style",style));
@@ -76,15 +80,25 @@ _ROOT_SYSTEM.ResolverUnaUnidadUI= function(xmlUnidadUI){
 			obj.innerHTML=propiedades[i].children[0].getAttribute("valor");
 		});
 	}
+	if(unTipoUI.getAttribute("es_recargable_db")=="1"){
+		document.head.appendChild(CrearDom("script","var Fn_Repeat_" + this.div_repeat + "_" + opx + "=" + xmlUnidadUI.getAttribute("callback_rec_db")));
+		var opx1= xmlUnidadUI.getAttribute("opx_rec_db");		
+		nuevo_dom.append(CrearDom("script","_ROOT_SYSTEM.CargarInfoUnidadUI('" + unTipoUI.getAttribute("obj_recargable_db"), opx1 + "');"));
+	}
+
+	
+
 	/*nuevo_dom.style= GetValAttr(xmlinfo,"evento");
 	var atributos=$(xmlinfo).find(" > atributo");
 	for(var i=0;i<atributos.length;i++){
-		nuevo_dom.setAttribute(GetValAttr(atributos[i],"nombre"),GetValAttr(atributos[i],"valor"));
+		nuevo_dom.setAttribute(GetValAttr(atributos[i],"nombre"),GetValtAtr(atributos[i],"valor"));
 	}
 	var eventos=$(xmlinfo).find(" > eventos");
 	for(var i=0;i<eventos.length;i++){
 		nuevo_dom.addEventListener(GetValAttr(eventos[i],"evento"),GetValAttr(eventos[i],"metodo"));
 	}*/
+
+
 	return nuevo_dom;
 }
 
