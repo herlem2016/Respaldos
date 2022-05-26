423f145b552218e2c2c064a37eb57f866b8e8e04
herlem2016 <herlem.kx@gmail.com>
2022-05-25 21:30:27 -0500

    Add files via upload

--- js/root.js
+++ js/root.js
@@ -16,6 +16,19 @@ var GetValAttr= function(nodo,attr){
 	return $(nodo).attr(attr);
 }
 
+var GetVal= function(nodo,subnodo,decode){
+	var val="";
+	var items=$(nodo).children(subnodo);
+	if(items.length>0){
+		if(decode){		
+			val=decodeHTMLEntities($(items[0]).html());
+		}else{
+			val= $(items[0]).html();
+		}
+	}
+	return val;
+}
+
 function decodeHTMLEntities(text) {
 	if(text){
 		var entities = [
@@ -43,46 +56,43 @@ _ROOT_SYSTEM= function(){}
 
 _ROOT_SYSTEM.ConstruirUI= function(url,vista,dom_contenedor){
 	dom_contenedor= GetDom(dom_contenedor);
-	$.post(url,{seccion:"generic",op:"ObtenerUI",vista:vista},function(xmlresp){
-		dom_contenedor.appendChild(_ROOT_SYSTEM.ProcesarArbolUnidadesUI(xmlresp));		
+	$.post(url,{seccion:"generic",op:"ObtenerUI",vista:vista},function(xmlresp){	
+		dom_contenedor.appendChild(_ROOT_SYSTEM.ProcesarVista(xmlresp));	
 	});
 }
 
-_ROOT_SYSTEM.ProcesarArbolUnidadesUI= function(xmlinfo){
-	var nuevo_dom;
-	var unidadesUI=$(xmlinfo).find("ui > unidadUI");
-	if(unidadesUI.length==0){
-		nuevo_dom=_ROOT_SYSTEM.ResolverUnaUnidadUI(xmlinfo);
-	}else{
-		nuevo_dom= document.createElement("div");
-		for(var i=0;i<unidadesUI.length;i++){
-			nuevo_dom.appendChild(_ROOT_SYSTEM.ProcesarArbolUnidadesUI(unidadesUI[i]));
-		}
+_ROOT_SYSTEM.ProcesarVista= function(xmlinfo){
+	//alert((new XMLSerializer()).serializeToString(xmlinfo));
+	var nuevo_dom= CrearDom("div",$(xmlinfo).find("Vista > template")[0]);
+	var unidadesUI=$(nuevo_dom).find("ui[nel-ui]");
+	for(var i=0;i<unidadesUI.length;i++){		
+		_ROOT_SYSTEM.ResolverUnaUnidadUI(unidadesUI[i],$(xmlinfo).children("ui[indice=" + unidadesUI[i].getAttribute("nel-ui") +"]"));
 	}
 	return nuevo_dom;
 }
 
-_ROOT_SYSTEM.CargarInfoUnidadUI= function(sdiv_repeat, opx){
-	$.post({url:"logic/controlador.aspx?op=GenerarOperacionCX_DB&seccion=generic&opx=" + opx, succes:function(data,xqr){ eval("Fn_Repeat_" + sdiv_repeat + "_" + opx + "(data,xqr,$('#" + sdiv_repeat + "'));"); }});
-}
-
-_ROOT_SYSTEM.ResolverUnaUnidadUI= function(xmlUnidadUI){
-	var unTipoUI=$(xmlUnidadUI).find("tipoUnidadUI")[0];
-	var style=GetValAttr(unTipoUI,"css");if(style) document.head.appendChild(CrearDom("style",style));
-	var script=GetValAttr(unTipoUI,"javascript");if(script) document.head.appendChild(CrearDom("script",script));
-	var nuevo_dom= document.createElement("div");
-	nuevo_dom.className= GetValAttr(unTipoUI,"class");	
-	nuevo_dom.innerHTML=decodeHTMLEntities(GetValAttr(unTipoUI,"innerHTML"));
-	var item_repeat=GetValAttr(unTipoUI,"item_repeat"),wrap=GetValAttr(unTipoUI,"wrap");
-	eval("var data=" + decodeHTMLEntities(xmlUnidadUI.getAttribute('data'))+";");
-	if(item_repeat){
-		if(data) ProcesarRepeticionItem(nuevo_dom.children[0],item_repeat,data,wrap);
-		else{
+_ROOT_SYSTEM.ResolverUnaUnidadUI= function(unidadUI,xmlUnidadUI){
+	eval("var data=" + GetVal(xmlUnidadUI,'data',true)+";");
+	if(data){
+		if(data){
+			unidadUI.appendChild(_ROOT_SYSTEM.ProcesarUnaUnidadUI(xmlUnidadUI,data));
+		}else{
+			var 
 			$.post({url:"logic/controlador.aspx?opx=" + xmlUnidadUI.getAttribute("opx"), succes:function(xdata,xqr){
-				ProcesarRepeticionItem(nuevo_dom.children[0],item_repeat,xdata,wrap);
+				unidadUI.appendChild(_ROOT_SYSTEM.ProcesarUnaUnidadUI(xmlUnidadUI,xdata));
 			}});
 		}
-	}
+	}	
+}
+
+_ROOT_SYSTEM.ProcesarUnaUnidadUI= function(xmlUnidadUI, data){
+	var style=GetVal(xmlUnidadUI,"css");if(style) document.head.appendChild(CrearDom("style",style));
+	var script=GetVal(xmlUnidadUI,"javascript");if(script) document.head.appendChild(CrearDom("script",script));
+	var nuevo_dom= document.createElement("div");
+	nuevo_dom.className= GetVal(xmlUnidadUI,"class");	
+	nuevo_dom.innerHTML=GetVal(xmlUnidadUI,"innerHTML",true);
+	var wrap=xmlUnidadUI.children("wrap");	
+	if(wrap.length>0) _ROOT_SYSTEM.ProcesarRepeticionItem(nuevo_dom.children[0],item_repeat,data,wrap);		
 	//Obtenemos las propiedades del TipoUI
 	/*var propiedades=$(unTipoUI).find("propiedad");
 	for(var i=0;i<propiedades.length;i++){
@@ -108,70 +118,36 @@ _ROOT_SYSTEM.ResolverUnaUnidadUI= function(xmlUnidadUI){
 	for(var i=0;i<eventos.length;i++){
 		nuevo_dom.addEventListener(GetValAttr(eventos[i],"evento"),GetValAttr(eventos[i],"metodo"));
 	}*/
-
-
 	return nuevo_dom;
 }
 
 
-function ProcesarRepeticionItem(container, _item_repeat, data,wrap){
-	if(data.length>0){	
+function _ROOT_SYSTEM.ProcesarRepeticionItem(container, data, xmlUnidadUI){
+	if(data.length>0){
+		var item_repeat=GetVal(xmlUnidadUI,"item_repeat",true);
+		var wrap=GetVal(xmlUnidadUI,"wrap",true);	
 		var nodowrap= document.createElement("div");
 		nodowrap.innerHTML=wrap;	
-		nodowrap= container.appendChild(nodowrap.children[0]);
-		var w=_item_repeat.match(/(?:^|\W)@(\w+)(?!\w)/g);
-		var child,item_repeat,child;
-		for(var n=0; n<data.length;n++){
-			item_repeat= _item_repeat;
-			for(var i=0;i<w.length;i++){
-				var w_i=w[i].substring(w[i].indexOf("@"),w[i].length).trim();
-				item_repeat=item_repeat.replace(w_i,data[n][w_i]);
-			}
-			child= document.createElement("div");
-			child.innerHTML=item_repeat;
-			child=nodowrap.appendChild(child.children[0]);
-			if(data[n].items !=null && data[n].items.length>0){				
-				ProcesarRepeticionItem(child,_item_repeat,data[n].items,wrap);
+		if(nodowrap.children[0]){
+			nodowrap= container.appendChild(nodowrap.children[0]);
+			var w=_item_repeat.match(/(?:^|\W)@(\w+)(?!\w)/g);
+			var child,item_repeat,child;
+			for(var n=0; n<data.length;n++){
+				item_repeat= _item_repeat;
+				for(var i=0;i<w.length;i++){
+					var w_i=w[i].substring(w[i].indexOf("@"),w[i].length).trim();
+					item_repeat=item_repeat.replace(w_i,data[n][w_i]);
+				}
+				child= document.createElement("div");
+				child.innerHTML=item_repeat;
+				child=nodowrap.appendChild(child.children[0]);	
+				if(data[n].items !=null && data[n].items.length>0){				
+					nuevo_dom.appendChild(_ROOT_SYSTEM.ProcesarUnaUnidadUI($(xmlUnidadUI).children("Layout > ui")[0],data[n].items));
+				}				
 			}
 		}
 	}	
 }
-/*
-function ProcesarRepeticionItem(container, dom_model, xml_data,wrap,enable_wrap){	
-	var nodos=$(xml_data).child(".item");
-	var nodowrap=container;
-	if(nodos.length>0){		
-		if(!enable_wrap){
-			wrap=container.outerHTML;
-			nodowrap=container;
-		}else{
-			nodowrap= document.createElement("div");
-			nodowrap.innerHTML=wrap;
-			container.appendChild(nodowrap.firstChild);
-			var nodos_cont=container.children;
-			if(nodos_cont.length>0){
-				container=$(nodowrap).find(".container")[0];
-				if(!container){
-					container=nodos_cont[nodos_cont.length-1];
-				}
-			}
-		}
-		var w=item_repeat.match(/(?:^|\W)@(\w+)(?!\w)/g);
-		var child,item_repeat,child;
-		for(var n=0; n<nodos.length;n++){
-			item_repeat= dom_model;
-			for(var i=0;i<w.length;i++){
-				item_repeat=item_repeat.replace(w[i],GetValor(nodos[n],w[i]));
-			}
-			child= document.createElement("div");
-			child.innerHTML=item_repeat;
-			nodowrap.appendChild(child.firstChild);
-			if($(nodos[i]).child("item")>0){				
-				ProcesarRepeticionItem(wrap, child.firstChild,dom_model, nodos[i],true);
-			}
-		}
-	}	
-}*/
 
 window.onload= function(){
 	var url_base= document.body.getAttribute("url");
